3de3061b14623c2e97b61cff31f2223182d18879
james kwok <kinkwanjames@gmail.com>
2022-11-16 10:58:19 -0500

    add dotenv dependency & .env

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
-const port = 8080;
 const videosRouter = require("./routes/videos");
+
+require('dotenv').config()
+
+const { PORT, BACKEND_URL } = process.env
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/videos", videosRouter);
 
-app.listen(port, () => console.log("API Server is running..."));
+app.listen(PORT, () => console.log("API Server is running..."));
