a353b258c54078f1fdc4c77c8c02ee46897c4db6
james kwok <kinkwanjames@gmail.com>
2022-11-16 10:34:39 -0500

    add cors dependency

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const port = 8080;
 const videosRouter = require("./routes/videos");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/videos", videosRouter);
