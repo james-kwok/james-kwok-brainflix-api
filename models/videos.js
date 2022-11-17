e2f050afc9a2eb4f5fbdbbb4314450f0084f9b80
james kwok <kinkwanjames@gmail.com>
2022-11-16 19:23:23 -0500

    create post feature for upload video

--- models/videos.js
+++ models/videos.js
@@ -1,14 +1,19 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const filePath = path.join(__dirname, "../data/videos.json");
 
 function getVideos() {
-    const videosJson = fs.readFileSync(filePath);
+  const videosJson = fs.readFileSync(filePath);
 
-    return JSON.parse(videosJson);
+  return JSON.parse(videosJson);
+}
+
+function saveVideo(video) {
+  fs.writeFileSync(filePath, JSON.stringify(video));
 }
 
 module.exports = {
-    getVideos,
-}
+  getVideos,
+  saveVideo
+};
--- routes/videos.js
+++ routes/videos.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { getVideos } = require("../models/videos");
+const { v4: uuidv4 } = require('uuid');
+const { getVideos, saveVideo } = require("../models/videos");
 
 // getting videos for videoList
 
 router.get("/", (req, res) => {
   const videos = getVideos();
-  const videoList = videos.map(({id, title, channel, image}) => {
-    return {id, title, channel, image}
-  })
-  res.json(videoList);
+  const videoList = videos.map(({ id, title, channel, image }) => {
+    return { id, title, channel, image };
+  });
+  res.status(200).json(videoList);
 });
 
 // getting a video for featuredVideo
@@ -26,9 +27,39 @@ router.get("/:id", (req, res) => {
       error: "Video does not exist.",
     });
   }
-  return res.json(foundVideo);
+  return res.status(200).json(foundVideo);
 });
 
 // upload video
+
+router.post("/", (req, res) => {
+  if (!req.body.title || !req.body.description) {
+    return res.status(400).json({
+      message: "All fields are required",
+    });
+  }
+
+  const newVideo = {
+    id: uuidv4(),
+    title: req.body.title,
+    channel: "Red Cow",
+    image: "http://localhost:8080/images/image9.jpg",
+    description: req.body.description,
+    views: 0,
+    likes: 0,
+    duration: "4:20",
+    video: "https://project-2-api.herokuapp.com/stream",
+    timestamp: 1626032763000,
+    comments: []
+}
+
+const videos = getVideos();
+videos.push(newVideo);
+
+saveVideo(videos);
+
+// response 
+res.status(201).json(newVideo);
+});
 
 module.exports = router;
